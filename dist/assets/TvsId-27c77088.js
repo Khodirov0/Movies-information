37f5def2db221693a9d586527be8644088738cc8
Suhrob Qodirov <123749861+Khodirov0@users.noreply.github.com>
2025-02-28 13:43:52 +0500

    Deploy

---
+++ dist/assets/TvsId-27c77088.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./ContentId-d39dc170.js";import{a,c as t,b as s}from"./index-95bdbdba.js";const c={class:"main"},p={__name:"TvsId",setup(o){return(_,r)=>(a(),t("main",c,[s(e,{type:"tv"})]))}};export{p as default};
